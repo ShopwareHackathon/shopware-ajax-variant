6281669fed3b88a732868a3a61a9c8cc245d984e
klarstil <klarstil@googlemail.com>
2015-08-05 15:20:57 +0200

    Initial import

--- themes/Frontend/Responsive/Theme.php
+++ themes/Frontend/Responsive/Theme.php
@@ -97,6 +97,7 @@ class Theme extends \Shopware\Components\Theme
         'src/js/jquery.image-gallery.js',
         'src/js/jquery.offcanvas-html-panel.js',
         'src/js/jquery.jump-to-tab.js',
+        'src/js/jquery.ajax-variant.js',
         'src/js/jquery.shopware-responsive.js'
     ];
 
