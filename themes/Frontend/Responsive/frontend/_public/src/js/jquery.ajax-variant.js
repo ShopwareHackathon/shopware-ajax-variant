7805e14343e338627e8e35a9f7828f7edc9250a7
klarstil <klarstil@googlemail.com>
2015-08-05 18:03:55 +0200

    The plugin now supports multiple get parameters and the reset button works with AJAX as well

--- themes/Frontend/Responsive/frontend/_public/src/js/jquery.ajax-variant.js
+++ themes/Frontend/Responsive/frontend/_public/src/js/jquery.ajax-variant.js
@@ -8,15 +8,21 @@
             var me = this;
 
             $('body').on(me.getEventName('click'), '*[data-ajax-variants="true"]', $.proxy(me.onChange, me));
+            $('body').on(me.getEventName('click'), '.reset--configuration', $.proxy(me.onChange, me));
         },
 
-        requestData: function($form) {
+        requestData: function(values) {
             var me = this,
-                location = window.location.href;
+                location = window.location.href,
+                separator = '?';
+
+            if(location.split('?').length > 1) {
+                separator = '&';
+            }
 
             $.ajax({
-                url: window.location.href + '?template=ajax',
-                data: $form.serialize(),
+                url: window.location.href + separator + 'template=ajax',
+                data: values || '',
                 method: 'POST',
                 success: function(response) {
                     var $response = $($.parseHTML(response)),
@@ -36,11 +42,12 @@
 
         onChange: function(event) {
             var me = this,
-                $target = $(event.target);
+                $target = $(event.target),
+                values = $target.parents('form').serialize();
 
             event.preventDefault();
 
-            me.requestData($target.parents('form'));
+            me.requestData(values);
             $.loadingIndicator.open({
                 closeOnClick: false
             });
