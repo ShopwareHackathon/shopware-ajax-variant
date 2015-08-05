be9839dcb227756e796aaa95b6aeeffb418b0044
klarstil <klarstil@googlemail.com>
2015-08-05 15:40:54 +0200

    Added plugin

--- themes/Frontend/Responsive/frontend/_public/src/js/jquery.ajax-variant.js
+++ themes/Frontend/Responsive/frontend/_public/src/js/jquery.ajax-variant.js
@@ -6,6 +6,35 @@
 
         init: function() {
             var me = this;
+
+            me._on(me.$el, 'click', $.proxy(me.onChange, me));
+        },
+
+        requestData: function(data) {
+            $.ajax({
+                url: window.location.href,
+                data: data,
+                method: 'POST',
+                success: function(response) {
+                    var $response = $(response),
+                        $productDetails = $response.find('.product--details');
+
+                    console.log($response.html());
+
+                    $('.product--details').html($productDetails.html());
+                }
+            });
+        },
+
+        onChange: function(event) {
+            var me = this,
+                $target = $(event.target),
+                group = $target.attr('name'),
+                value = $target.val();
+
+            event.preventDefault();
+
+            me.requestData(group + '=' + value);
         }
     });
 
